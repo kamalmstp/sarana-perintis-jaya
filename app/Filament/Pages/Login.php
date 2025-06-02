0ae577a4b779efc2b967a72af1aa5efa89e9c4a1
Nuremax <mustapakamalkml@gmail.com>
2025-06-02 11:35:49 +0800

    login rev

--- app/Filament/Pages/Login.php
+++ app/Filament/Pages/Login.php
@@ -61,8 +61,8 @@ class Login extends BaseLogin
         parent::mount();
 
         $this->form->fill([
-            'email' => 'admin@admin.com',
-            'password' => 'password',
+            'email' => '',
+            'password' => '',
             'remember' => true,
         ]);
     }
